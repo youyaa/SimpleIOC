d5c9d3a1995fad25ce870ae0c28ac130aa2d7d78
youyaa <listeningrain.cn@gmail.com>
2019-01-05 20:20:12 +0800

    去除多余的注解

---
+++ ListeningrainBlog-master/ListeningrainBlog/ListeningrainBlog_rest/src/main/java/listeningrain/cn/blog/Application.java
@@ -0,0 +1,37 @@
+package listeningrain.cn.blog;
+
+import listeningrain.cn.blog.utils.StorageProperties;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ImportResource;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * author: listeningrain
+ * Date: 2018/9/16
+ * Time: 15:0
+ * Description:Rest工程启动类
+ */
+@SpringBootApplication
+@ImportResource({"classpath:application-consumer.xml"})
+@EnableConfigurationProperties(StorageProperties.class)
+public class Application {
+
+    public static void main(String[] args){
+        printToConsole("开始启动listeningrainBlog-rest模块");
+        SpringApplication.run(Application.class);
+        printToConsole("启动listeningrainBlog-rest模块成功");
+    }
+
+    private static void printToConsole(String s) {
+        if (null != s && s.length() > 0) {
+            String time = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+            System.out.println(time + " " + s);
+        } else {
+            System.out.println(s);
+        }
+    }
+}
